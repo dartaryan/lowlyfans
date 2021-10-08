9a98109d8479e2f34404cf517fe792060a6d85c2
dartaryan <76628877+dartaryan@users.noreply.github.com>
2021-10-09 00:03:59 +0300

    create categories

---
+++ src/data.js
@@ -0,0 +1,46 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://cdna.artstation.com/p/assets/images/images/021/675/430/large/stef-walker-ivy-render.jpg?1572540313",
+    title: "Treat Or Trick?",
+    desc: "How about both? Get the best fan art halloween themed today!",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "https://i.pinimg.com/originals/bc/ae/76/bcae76c6a435d4d778a867f0197bf5fb.jpg",
+    title: "That wall is kind of empty...",
+    desc: "This could look great on it.",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "https://i.pinimg.com/originals/9a/9f/ee/9a9fee0eec6640c6b9f6046faf6d198c.jpg",
+    title: "Spooky Times!",
+    desc: "Time for some new decorations",
+    bg: "fbf0f4",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: "https://static.wikia.nocookie.net/rickandmorty/images/7/70/Rick_and_Morty_Season_1.jpg/revision/latest?cb=20161125224650",
+    title: "Rick and Morty",
+  },
+  {
+    id: 2,
+    img: "https://pm1.narvii.com/7487/d00bcd674ad9f2add8448b6f5dee7b8ce5a0ab73r1-2048-1433v2_hq.jpg",
+    title: "Pokemon",
+  },
+  {
+    id: 3,
+    img: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/446a20cc-3d9c-4cdf-8845-cee8c49b5076/dcbyl0g-be967860-1994-4f3e-97f2-a7f552cf5b7a.jpg/v1/fill/w_960,h_539,q_75,strp/dragon_ball_z_wallpaper_by_joshua121penalba_dcbyl0g-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NTM5IiwicGF0aCI6IlwvZlwvNDQ2YTIwY2MtM2Q5Yy00Y2RmLTg4NDUtY2VlOGM0OWI1MDc2XC9kY2J5bDBnLWJlOTY3ODYwLTE5OTQtNGYzZS05N2YyLWE3ZjU1MmNmNWI3YS5qcGciLCJ3aWR0aCI6Ijw9OTYwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.vF8kzCRQEdbYyQ-vo98KI39m-X6PJMs8S53woL2zOg0",
+    title: "Dragon Ball Z",
+  },
+  {
+    id: 4,
+    img: "https://coolthemestores.com/wp-content/uploads/2020/11/harry-potter-features.jpg",
+    title: "Harry Potter",
+  },
+];
